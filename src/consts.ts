ecff635d8f274629a66741a46cd3ea188c0d2a0f
like <1774124483@qq.com>
2024-03-15 11:47:28 +0800

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "🌈聿森的博客";
+export const SITE_DESCRIPTION = "生活分享 | 学习笔记 | 技术分享 | 个人博客";
+export const SITE_NAME_EN = "yusen";
+export const SITE_NAME_ZH_CN = "聿森";
+export const SITE_FILING = "鄂ICP备2024041115号-1";
+
+export const TWITTER_URL = "https://twitter.com/yusen_";
